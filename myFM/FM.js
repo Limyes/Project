74bcf53d9ab2d68b87ef36016eab363fefaea550
Limyes <975096654@qq.com>
2018-07-31 19:52:54 +0800

    [fix]

--- myFM/FM.js
+++ myFM/FM.js
@@ -60,7 +60,7 @@ var Footer={
 		})
 		this.$footer.on('click','li',function(){
 			$(this).addClass('active').siblings().removeClass('active')
-			EventCenter.fire('select-albumn',$(this).attr('data-channel-id'))
+			EventCenter.fire('select-albumn',{channelId:$(this).attr('data-channel-id'),channelName:$(this).attr('data-channel-name')})
 		})
 	},
 	render:function(){
@@ -75,7 +75,7 @@ var Footer={
 	renderFooter:function(channels){
 		var html=''
 		channels.forEach(function(channel){
-			html+='<li data-channel-id='+channel.channel_id+'>'
+			html+='<li data-channel-id='+channel.channel_id+' data-channel-name='+channel.name+'>'
 				+'<div class="cover" style="background:url('+channel.cover_small+') center center no-repeat;background-size:cover;"></div>'
 				+'<h3>'+channel.name+'</h3>'+'</li>'
 		})
@@ -85,31 +85,93 @@ var Footer={
 	setStyle:function(){
 		var count=this.$footer.find('li').length
 		var width=this.$footer.find('li').outerWidth(true)
-		this.$ul.css({'width':count*width+'px'})
+		this.$ul.css({'widths':count*width+'px'})
 	}
 }
 var Fm={
 	init:function(){
+		this.$container=$('#page-music')
+		this.audio=new Audio()
+		this.audio.autoplay=true
 		this.bindEvents()
 	},
 	bindEvents:function(){
 		var _this=this
-		EventCenter.on('select-albumn',function(e,channelId){
-			_this.channelId=channelId
-			_this.loadMusic(function(){
-				_this.setMusic()
-			})
+		EventCenter.on('select-albumn',function(e,channelObj){
+			_this.channelId=channelObj.channelId
+			_this.channelName=channelObj.channelName
+			_this.loadMusic()
+		})
+		this.$container.find('.btn-play').on('click',function(){
+			var $btn=$(this)
+			if($btn.hasClass('icon-bofang')){
+				$btn.removeClass('icon-bofang').addClass('icon-weibiaoti519')
+				_this.audio.play()
+			}else{
+				$btn.removeClass('icon-weibiaoti519').addClass('icon-bofang')
+				_this.audio.pause()
+			}
+		})
+		this.$container.find('.btn-next').on('click',function(){
+			_this.loadMusic()
+
+		})
+		this.audio.addEventListener('play',function(){
+			console.log('play...')
+			clearInterval(_this.statusClock)
+			_this.statusClock=setInterval(function(){
+				_this.updateStatus()
+			},1000)
+		})
+		this.audio.addEventListener('pause',function(){
+			clearInterval(_this.statusClock)
+			console.log('pause...')
 		})
 	},
 	loadMusic:function(callback){
 		var _this=this
 		$.getJSON('https://jirenguapi.applinzi.com/fm/getSong.php',{channel:this.channelId}).done(function(ret){
 			_this.song=ret['song'][0]
+			_this.setMusic()
+			_this.loadLyric()
 		})
-		callback()
+		
+	},
+	loadLyric:function(){
+		var _this=this
+		$.getJSON('https://jirenguapi.applinzi.com/fm/getLyric.php',{sid:this.song.sid}).done(function(ret){
+			var lyric=ret.lyric
+			var lyricObj={}
+			lyric.split('\n').forEach(function(line){
+				var times=line.match(/\d{2}:\d{2}/g)
+				var str=line.replace(/\[.+?\]/g,'')
+				if(Array.isArray(times)){
+					times.forEach(function(time){
+						lyricObj[time]=str
+					})
+				}
+			})
+			_this.lyricObj=lyricObj
+		})
 	},
 	setMusic:function(){
 		console.log('setmusic...')
+		console.log(this.song)
+		this.audio.src=this.song.url
+		$('.bg').css({"background":'url('+this.song.picture+') center center no-repeat',"background-size":"cover"})
+		this.$container.find('.music-cover figure').css("background",'url('+this.song.picture+') center center no-repeat')
+		this.$container.find('.music-info h1').text(this.song.title)
+		this.$container.find('.music-info .author').text(this.song.artist)
+		this.$container.find('.music-info .tag').text(this.channelName)
+		this.$container.find('.btn-play').removeClass('icon-bofang').addClass('icon-weibiaoti519')
+	},
+	updateStatus:function(){
+		var min=Math.floor(this.audio.currentTime/60)
+		var second=Math.floor(this.audio.currentTime%60)+''
+		second=second.length===2?second:'0'+second
+		this.$container.find('.current-time').text(min+':'+second)
+		this.$container.find('.bar-progress').css('width',this.audio.currentTime/this.audio.duration*100+'%')
+		console.log('update')
 	}
 }
 
